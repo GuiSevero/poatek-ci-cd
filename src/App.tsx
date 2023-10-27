93aa03f18465526ccf9e50ee64846a818ca2bb5b
Guilherme Severo <guilherme.severo@poatek.com>
2023-10-27 14:42:20 -0300

    first

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Hello, CI/CD</h1>
+        <div>
+        {count}
+        </div>
+        <button onClick={() => setCount(count + 1)}>+</button>
+        </div>
+    </>
+  )
+}
+
+export default App
